355bb88b571bf15615015142a45a061d0eae65a0
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-30 23:19:09 +0600

    login form ui done

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
+        <div className=''>
             <h2 className='text-5xl' > LogIn</h2>
+            <form className='flex flex-col '>
+            <input type="text" placeholder="email" class="input input-bordered w-full max-w-xs" />
+            <input type="text" placeholder="password" class="input input-bordered w-full max-w-xs" />
+            <input value='Login' class=" btn w-full max-w-xs" />
+
+            </form>
         </div>
     );
 };
